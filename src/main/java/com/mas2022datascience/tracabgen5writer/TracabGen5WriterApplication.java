4d16284e3993e50f6d637124fda12ca835ebadcd
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-12-27 22:26:37 +0100

    fix utc time

--- src/main/java/com/mas2022datascience/tracabgen5writer/TracabGen5WriterApplication.java
+++ src/main/java/com/mas2022datascience/tracabgen5writer/TracabGen5WriterApplication.java
@@ -81,80 +81,12 @@ public class TracabGen5WriterApplication implements CommandLineRunner {
 
 			// Read the file line by line
 			String line;
+			line = br.readLine();
+			final long initialFrameNumber = Long.parseLong(line.split(":")[0]);
+			processData(line, metadata, initialFrameNumber);
+
 			while ((line = br.readLine()) != null) {
-
-				String[] lineSplit = line.split(":");
-				// chunk 1 is the offset counter
-				long timeOffsetInMs = Integer.parseInt(lineSplit[0]) * 1000 / metadata.getFrameRate();
-
-				// chunk 2 contains the player and referee data
-				// Data type: String represented array of up to 29 objects
-				// Each object contains the following properties:
-				// (0) targets assigned team*, (1) system target ID, (2) assigned jersey** number,
-				// (3) pitch position x***, (4) pitch position y***, (5) target speed****
-				ArrayList<Object> objects = new ArrayList<>();
-				String[] chunk2 = lineSplit[1].split(";");
-				for (int i = 0; i < chunk2.length-1; i++) {
-					String[] objectData = chunk2[i].split(",");
-					if (!objectData[0].equals("-1")) {
-						objects.add(Object.newBuilder()
-								.setType(Integer.parseInt(objectData[0]))
-								.setId(objectData[2])
-								.setX(Integer.parseInt(objectData[3]))
-								.setY(Integer.parseInt(objectData[4]))
-								.setZ(0)
-								.setSampling(0)
-								.setVelocity(Double.parseDouble(objectData[5]))
-								.build());
-					}
-				}
-
-				// chunk 3 contains the ball data and the optional data
-				// Object contains the following properties: (0) pitch position x*, (1) pitch position y*,
-				// (2) pitch position z*, (3) ball speed**,
-				// (4) ball owning team*** -> "H" (home) or "A" (away).
-				// (5) ball status**** -> "Alive" or "Dead".
-				// (6) (not always set) ball contact device info 1 ***** -> ignored
-				// (7) (not always set) ball contact device info 2 ***** -> ignored
-				String[] chunk3 = lineSplit[2].replace(";","").split(",");
-
-				objects.add(Object.newBuilder()
-						.setType(7)
-						.setId("0")
-						.setX(Integer.parseInt(chunk3[0]))
-						.setY(Integer.parseInt(chunk3[1]))
-						.setZ(Integer.parseInt(chunk3[2]))
-						.setSampling(0)
-						.setVelocity(Double.parseDouble(chunk3[3]))
-						.build());
-
-				String ballOwningTeam = "";
-				String isBallInPlay = "";
-				String ballContactDevice1 = "";
-
-				switch (chunk3.length) {
-					case 5 -> ballOwningTeam = chunk3[4];
-					case 6 -> {
-						ballOwningTeam = chunk3[4];
-						isBallInPlay = chunk3[5];
-					}
-					case 7 -> {
-						ballOwningTeam = chunk3[4];
-						isBallInPlay = chunk3[5];
-						ballContactDevice1 = chunk3[6];
-					}
-					default -> {
-					}
-				}
-
-				kafkaTracabProducer.produce(String.valueOf(metadata.getGameID()), TracabGen5TF01.newBuilder()
-						.setUtc(Instant.ofEpochMilli(Instant.parse(initialTime).toEpochMilli() + timeOffsetInMs).atZone(ZoneOffset.UTC).toString())
-						.setBallPossession(ballOwningTeam)
-						.setIsBallInPlay(isBallInPlay)
-						.setContactDevInfo(ballContactDevice1)
-						.setObjects(objects)
-						.build());
-
+				processData(line, metadata, initialFrameNumber);
 			}
 
 			// Close the file
@@ -163,4 +95,81 @@ public class TracabGen5WriterApplication implements CommandLineRunner {
 			e.printStackTrace();
 		}
 	}
+
+	private void processData(String line, TracabGen5TF01Metadata metadata, long initialFrameNumber) {
+		String[] lineSplit = line.split(":");
+		// chunk 1 is the offset counter
+		long timeOffsetInMs = (Long.parseLong(lineSplit[0])-initialFrameNumber) * (1000 / metadata.getFrameRate());
+
+		// chunk 2 contains the player and referee data
+		// Data type: String represented array of up to 29 objects
+		// Each object contains the following properties:
+		// (0) targets assigned team*, (1) system target ID, (2) assigned jersey** number,
+		// (3) pitch position x***, (4) pitch position y***, (5) target speed****
+		ArrayList<Object> objects = new ArrayList<>();
+		String[] chunk2 = lineSplit[1].split(";");
+		for (int i = 0; i < chunk2.length-1; i++) {
+			String[] objectData = chunk2[i].split(",");
+			if (!objectData[0].equals("-1")) {
+				objects.add(Object.newBuilder()
+						.setType(Integer.parseInt(objectData[0]))
+						.setId(objectData[2])
+						.setX(Integer.parseInt(objectData[3]))
+						.setY(Integer.parseInt(objectData[4]))
+						.setZ(0)
+						.setSampling(0)
+						.setVelocity(Double.parseDouble(objectData[5]))
+						.build());
+			}
+		}
+
+		// chunk 3 contains the ball data and the optional data
+		// Object contains the following properties: (0) pitch position x*, (1) pitch position y*,
+		// (2) pitch position z*, (3) ball speed**,
+		// (4) ball owning team*** -> "H" (home) or "A" (away).
+		// (5) ball status**** -> "Alive" or "Dead".
+		// (6) (not always set) ball contact device info 1 ***** -> ignored
+		// (7) (not always set) ball contact device info 2 ***** -> ignored
+		String[] chunk3 = lineSplit[2].replace(";","").split(",");
+
+		objects.add(Object.newBuilder()
+				.setType(7)
+				.setId("0")
+				.setX(Integer.parseInt(chunk3[0]))
+				.setY(Integer.parseInt(chunk3[1]))
+				.setZ(Integer.parseInt(chunk3[2]))
+				.setSampling(0)
+				.setVelocity(Double.parseDouble(chunk3[3]))
+				.build());
+
+		String ballOwningTeam = "";
+		String isBallInPlay = "";
+		String ballContactDevice1 = "";
+
+		switch (chunk3.length) {
+			case 5 -> ballOwningTeam = chunk3[4];
+			case 6 -> {
+				ballOwningTeam = chunk3[4];
+				isBallInPlay = chunk3[5];
+			}
+			case 7 -> {
+				ballOwningTeam = chunk3[4];
+				isBallInPlay = chunk3[5];
+				ballContactDevice1 = chunk3[6];
+			}
+			default -> {
+			}
+		}
+
+		kafkaTracabProducer.produce(String.valueOf(metadata.getGameID()), TracabGen5TF01
+				.newBuilder()
+				.setUtc(Instant.ofEpochMilli(Instant.parse(initialTime).toEpochMilli() + timeOffsetInMs).atZone(ZoneOffset.UTC).toString())
+				.setBallPossession(ballOwningTeam)
+				.setIsBallInPlay(isBallInPlay)
+				.setContactDevInfo(ballContactDevice1)
+				.setObjects(objects)
+				.build());
+
+	}
+
 }
