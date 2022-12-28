11647aa7f8ba489a2dc6918fdb419cb987e54480
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-12-28 17:20:49 +0100

    modify produce method to use topic variable

--- src/main/java/com/mas2022datascience/tracabgen5writer/producer/KafkaTracabProducer.java
+++ src/main/java/com/mas2022datascience/tracabgen5writer/producer/KafkaTracabProducer.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
@@ -15,14 +14,11 @@ public class KafkaTracabProducer {
   @Autowired
   private KafkaTemplate<String, TracabGen5TF01> kafkaTemplate;
 
-  @Value(value = "${topic.tracab.name}")
-  private String tracabTopic;
-
-  public void produce(String key, TracabGen5TF01 value) {
+  public void produce(String topic, String key, TracabGen5TF01 value) {
 
     SendResult<String, TracabGen5TF01> result = null;
     try {
-      result = kafkaTemplate.send(tracabTopic, key, value).get(10, TimeUnit.SECONDS);
+      result = kafkaTemplate.send(topic, key, value).get(10, TimeUnit.SECONDS);
     } catch (InterruptedException | ExecutionException | TimeoutException e ) {
       e.printStackTrace();
     }
